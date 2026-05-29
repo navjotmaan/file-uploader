8929056a8a9f37c2d0264a3db18181424ec338b2
navjotmaan <navjotmaan137@gmail.com>
2026-05-29 16:15:06 +0000

    add cors middleware

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,8 +6,14 @@ import { PrismaClient } from "../generated/prisma/client";
 import { PrismaSessionStore } from '@quixo3/prisma-session-store';
 import passport from 'passport';
 import './config/passport';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:5173', 
+  credentials: true                
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
