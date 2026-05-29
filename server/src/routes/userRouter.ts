87c4405680cb2bb83b5ea2663e24969f27368e39
navjotmaan <navjotmaan137@gmail.com>
2026-05-29 16:14:18 +0000

    add route to check if user logged in

--- server/src/routes/userRouter.ts
+++ server/src/routes/userRouter.ts
@@ -1,4 +1,4 @@
-import { register, login, logout } from '../controllers/userController.js';
+import { register, login, logout, ensureAuthenticated, checkUserAuth } from '../controllers/userController.js';
 import { Router } from 'express';
 const userRouter = Router();
 import passport from 'passport';
@@ -6,5 +6,7 @@ import passport from 'passport';
 userRouter.post('/register', register);
 userRouter.post('/login', passport.authenticate('local'), login);
 userRouter.post('/logout', logout);
+
+userRouter.get('/api/me', ensureAuthenticated, checkUserAuth);
 
 export { userRouter };
