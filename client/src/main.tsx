2c42ec1dbbc5eefd8f67ce8fc51f94a431e76e90
navjotmaan <navjotmaan137@gmail.com>
2026-05-31 09:10:04 +0000

    wrap the router inside UserProvider context and dashboard route inside protectedRoute

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,19 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App from './components/App.tsx'
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import { Dashboard } from './components/Dashboard.tsx'
 import { Login } from './components/forms/Login.tsx'
 import { Signup } from './components/forms/Signup.tsx'
-import { Error } from './components/Error.tsx'
+import { Error } from './components/helpers/Error.tsx'
+import { UserProvider } from './components/helpers/ContextApi.tsx'
+import { ProtectedRoute } from './components/helpers/ProtectedRoute.tsx'
 
 const router = createBrowserRouter([
   {
     errorElement: <Error />,
     children: [
       { path: '/', element: <App /> },
-      { path: '/dashboard', element: <Dashboard />},
+      { path: '/dashboard', element: <ProtectedRoute><Dashboard /></ProtectedRoute>},
       { path: 'login', element: <Login /> },
       { path: 'register', element: <Signup /> },
       
@@ -23,6 +25,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <UserProvider>
+      <RouterProvider router={router} />
+    </UserProvider>
   </StrictMode>,
 )
