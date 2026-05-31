34b81ba72b4002f46b4ab1e819e465e3e43e0d70
navjotmaan <navjotmaan137@gmail.com>
2026-05-31 09:15:11 +0000

    display folder form inside dashboard

--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -1,7 +1,11 @@
+import { FolderForm } from "./forms/Folder";
+
 export const Dashboard = () => {
+
     return (
         <div>
             <h1>File Uploader</h1>
+            <FolderForm />
         </div>
     )
 };
