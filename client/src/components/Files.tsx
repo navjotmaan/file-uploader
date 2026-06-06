8c58171fc570f35c6a1258128277b1d8d9ac37db
navjotmaan <navjotmaan137@gmail.com>
2026-06-06 16:06:10 +0000

    create a component to display all files

---
+++ client/src/components/Files.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react"
+import { FileForm } from "./forms/FileForm";
+import { useParams } from "react-router-dom";
+import api from "./api";
+
+export interface File {
+    id: string;
+    name: string;
+    url?: string;
+    size?: number;
+}
+
+export const Files = () => {
+    const [files, setFiles] = useState<File[]>([]);
+    const { id } = useParams<{ id: string }>();
+
+    useEffect(() => {
+        if (!id) return;
+
+        (async () => {
+            try {
+                const res = await api.get(`/file/${id}/all`);
+                setFiles(res.data);
+            } catch (err) {
+                console.log(err);
+            }
+        })();
+    }, [id]);
+
+    if (!id) {
+        return <div>Invalid Folder</div>;
+    }
+
+    return (
+        <div className="p-20">
+            <FileForm folderId={id} setFiles={setFiles} />
+            {files.map((f: File) => (
+                <div key={f.id} className="border rounded p-2 w-50">{f.name}</div>
+            ))}
+        </div>
+    )
+}
