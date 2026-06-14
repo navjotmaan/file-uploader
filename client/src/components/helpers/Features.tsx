6cb210c0da851d2543f714d3dfe8278d87c1f129
navjotmaan <navjotmaan137@gmail.com>
2026-06-14 16:24:05 +0000

    add features file

---
+++ client/src/components/helpers/Features.tsx
@@ -0,0 +1,20 @@
+export const Features = () => {
+  return (
+    <div>
+      <span className="flex items-center gap-3 mt-10">
+        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-lock-keyhole-icon lucide-lock-keyhole text-[#09a0d3]"><circle cx="12" cy="16" r="1"/><rect x="3" y="10" width="18" height="12" rx="2"/><path d="M7 10V7a5 5 0 0 1 10 0v3"/></svg>
+        <p className="text-lg font-semibold">End-to-End Encryption</p>
+      </span>
+
+      <span className="flex items-center gap-3 mt-5">
+        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-zap-icon lucide-zap text-[#09a0d3]"><path d="M4 14a1 1 0 0 1-.78-1.63l9.9-10.2a.5.5 0 0 1 .86.46l-1.92 6.02A1 1 0 0 0 13 10h7a1 1 0 0 1 .78 1.63l-9.9 10.2a.5.5 0 0 1-.86-.46l1.92-6.02A1 1 0 0 0 11 14z"/></svg>
+        <p className="text-lg font-semibold">Lightning-Fast Sharing</p>
+      </span>
+
+      <span className="flex items-center gap-3 mt-5">
+        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-server-icon lucide-server text-[#09a0d3]"><rect width="20" height="8" x="2" y="2" rx="2" ry="2"/><rect width="20" height="8" x="2" y="14" rx="2" ry="2"/><line x1="6" x2="6.01" y1="6" y2="6"/><line x1="6" x2="6.01" y1="18" y2="18"/></svg>
+        <p className="text-lg font-semibold">Free 15 GB Storage</p>
+      </span>
+    </div>
+  )
+};
