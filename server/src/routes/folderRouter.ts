669f4786e9dd6947fb4c0d7d7d782b70ded67c93
navjotmaan <navjotmaan137@gmail.com>
2026-07-07 15:36:20 +0000

    add route to delete folder

--- server/src/routes/folderRouter.ts
+++ server/src/routes/folderRouter.ts
@@ -1,9 +1,11 @@
 import { createFolder, getAllFolders } from "../controllers/folderController.js";
+import { deleteFoldersAndFiles } from "../controllers/fileController.js";
 import { Router } from "express";
 
 const folderRouter = Router();
 
 folderRouter.post('/new', createFolder);
 folderRouter.get('/all', getAllFolders);
+folderRouter.delete('/delete', deleteFoldersAndFiles);
 
 export { folderRouter };
