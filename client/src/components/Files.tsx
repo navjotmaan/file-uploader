aa2ec0369cbf52500886992916880f03e79afb33
navjotmaan <navjotmaan137@gmail.com>
2026-07-08 10:00:43 +0000

    add api req to delete file

--- client/src/components/Files.tsx
+++ client/src/components/Files.tsx
@@ -55,6 +55,15 @@ export const Files = () => {
         window.open(triggerDownloadUrl, '_blank');
     };
 
+    const deleteFile = async (fileId: string) => {
+        try {
+            await api.delete('/file/delete', { data: { fileId } });
+            setFiles((prevFiles) => prevFiles.filter((file) => file.id !== fileId));
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
     return (
         <div className="md:px-10 bg-[#e9ecef] min-h-screen py-10 px-5">
             <FileForm folderId={id} setFiles={setFiles} />
@@ -69,7 +78,11 @@ export const Files = () => {
                     </a>
                     <span className="flex justify-between items-center md:min-w-[35%]">
                         <p>{formatDate(f.updatedAt)}</p>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-download-icon lucide-download cursor-pointer" onClick={() => handleDownload(f.url, f.name)}><path d="M12 15V3"/><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/><path d="m7 10 5 5 5-5"/></svg>
+                        <span className="flex gap-5 items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-download-icon lucide-download cursor-pointer" onClick={() => handleDownload(f.url, f.name)}><path d="M12 15V3"/><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/><path d="m7 10 5 5 5-5"/></svg>
+
+                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-trash-icon lucide-trash cursor-pointer" onClick={() => deleteFile(f.id)}><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6"/><path d="M3 6h18"/><path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/></svg>
+                        </span>
                     </span>
                     <p>{f.size ? `${(f.size / 1024).toFixed(0)} KB` : "—"}</p>
                 </div>
